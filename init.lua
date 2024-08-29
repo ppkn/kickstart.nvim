cfc9db7743bbec48b39e283c62848ff313dee553
Daniel Pipkin <drpipkin@gmail.com>
2024-08-28 23:01:26 -0600

    tree netrw

--- init.lua
+++ init.lua
@@ -157,6 +157,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Tree view netrw
+vim.g.netrw_liststyle = 3
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
